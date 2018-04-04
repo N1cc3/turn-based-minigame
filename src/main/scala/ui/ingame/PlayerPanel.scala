ba5277ca26a6d43d0d411dacd1aae91b36cc2628
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-04-04 14:59:42 +0300

    Added some unit info to player panel.

--- src/main/scala/ui/ingame/PlayerPanel.scala
+++ src/main/scala/ui/ingame/PlayerPanel.scala
@@ -1,7 +1,8 @@
 package ui.ingame
 
+import scalafx.geometry.HPos
 import scalafx.scene.control.Label
-import scalafx.scene.layout.{HBox, VBox}
+import scalafx.scene.layout._
 import ui.Theme
 
 class PlayerPanel(theme: Theme) extends VBox {
@@ -69,5 +70,96 @@ class PlayerPanel(theme: Theme) extends VBox {
 	private val terrainNameLabel = new Label("Grass")
 	terrainNameLabel.style = theme("PlayerPanel.terrainNameLabel")
 	terrainBox.children.add(terrainNameLabel)
+
+	// Unit
+
+	private val unitBox = new VBox()
+	unitBox.style = theme("PlayerPanel.unitBox")
+	this.children.add(unitBox)
+
+	private val unitNameLabel = new Label("Ballista")
+	unitNameLabel.style = theme("PlayerPanel.unitNameLabel")
+	unitBox.children.add(unitNameLabel)
+
+	private val unitHpBox = new HBox()
+	unitHpBox.style = theme("PlayerPanel.unitHpBox")
+	unitBox.children.add(unitHpBox)
+
+	private val unitHpLabel = new Label("12 / 15")
+	unitHpLabel.style = theme("PlayerPanel.unitHpLabel")
+	unitBox.children.add(unitHpLabel)
+
+	private val unitStatsBox = new VBox()
+	unitStatsBox.style = theme("PlayerPanel.unitStatsBox")
+	unitBox.children.add(unitStatsBox)
+
+	private val attackBox = new HBox()
+	attackBox.style = theme("PlayerPanel.attackBox")
+	unitStatsBox.children.add(attackBox)
+
+	private val attackLabel = new Label("Attack")
+	attackLabel.style = theme("PlayerPanel.attackLabel")
+	attackBox.children.add(attackLabel)
+
+	private val attackValueLabel = new Label("0")
+	attackValueLabel.style = theme("PlayerPanel.attackValueLabel")
+	attackBox.children.add(attackValueLabel)
+
+	private val defenceBox = new HBox()
+	defenceBox.style = theme("PlayerPanel.defenceBox")
+	unitStatsBox.children.add(defenceBox)
+
+	private val defenceLabel = new Label("Defence")
+	defenceLabel.style = theme("PlayerPanel.defenceLabel")
+	defenceBox.children.add(defenceLabel)
+
+	private val defenceValueLabel = new Label("0")
+	defenceValueLabel.style = theme("PlayerPanel.defenceValueLabel")
+	defenceBox.children.add(defenceValueLabel)
+
+	private val armorBox = new HBox()
+	armorBox.style = theme("PlayerPanel.armorBox")
+	unitStatsBox.children.add(armorBox)
+
+	private val armorLabel = new Label("Armor")
+	armorLabel.style = theme("PlayerPanel.armorLabel")
+	armorBox.children.add(armorLabel)
+
+	private val armorValueLabel = new Label("0")
+	armorValueLabel.style = theme("PlayerPanel.armorValueLabel")
+	armorBox.children.add(armorValueLabel)
+
+	private val rangeBox = new HBox()
+	rangeBox.style = theme("PlayerPanel.rangeBox")
+	unitStatsBox.children.add(rangeBox)
+
+	private val rangeLabel = new Label("Range")
+	rangeLabel.style = theme("PlayerPanel.rangeLabel")
+	rangeBox.children.add(rangeLabel)
+
+	private val rangeValueLabel = new Label("10")
+	rangeValueLabel.style = theme("PlayerPanel.rangeValueLabel")
+	rangeBox.children.add(rangeValueLabel)
+
+
+	// TODO: Implement generic stat row like this:
+
+	val gridPane = new GridPane()
+	gridPane.style = theme("PlayerPanel.gridPane")
+	val column1 = new ColumnConstraints()
+	column1.setHgrow(Priority.Always)
+	val column2 = new ColumnConstraints()
+	column2.setHgrow(Priority.Always)
+	column2.setHalignment(HPos.Right)
+	gridPane.getColumnConstraints.addAll(column1, column2)
+	unitStatsBox.children.add(gridPane)
+
+	private val speedLabel = new Label("Speed")
+	speedLabel.style = theme("PlayerPanel.speedLabel")
+	gridPane.add(speedLabel, 0, 0)
+
+	private val speedValueLabel = new Label("10000")
+	speedValueLabel.style = theme("PlayerPanel.speedValueLabel")
+	gridPane.add(speedValueLabel, 1, 0)
 
 }
