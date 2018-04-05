7d69bcfe2924df2ea4107f60fc5dbb6940938a38
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-04-06 00:06:08 +0300

    Selection hex can be moved.

--- src/main/scala/ui/ingame/InGameScene.scala
+++ src/main/scala/ui/ingame/InGameScene.scala
@@ -7,7 +7,26 @@ import scalafx.scene.Scene
 import scalafx.scene.input.KeyCode
 import scalafx.scene.layout.{HBox, VBox}
 
+import scala.math.{max, min}
+
 class InGameScene(mod: Mod) extends Scene {
+
+	private var _selectionX: Int = 0
+	private var _selectionY: Int = 0
+
+	def selectionX: Int = _selectionX
+
+	def selectionY: Int = _selectionY
+
+	def selectionX_=(value: Int) {
+		_selectionX = min(gameState.map.sizeX - 1, max(0, value))
+		drawGame()
+	}
+
+	def selectionY_=(value: Int) {
+		_selectionY = min(gameState.map.sizeY - 1, max(0, value))
+		drawGame()
+	}
 
 	private val mainBox = new HBox()
 	this.content.add(mainBox)
@@ -32,36 +51,38 @@ class InGameScene(mod: Mod) extends Scene {
 	playerPanel2.styleClass += "InGameScene-playerPanel2"
 	mainBox.getChildren.add(playerPanel2)
 
+	def drawGame() {
+		canvas.drawMap(gameState.map)
+		canvas.drawUnits(gameState.units.toList, gameState.map)
+		canvas.drawSelection(gameState.map, new Hex(selectionX, selectionY))
+	}
+
 	// Listeners
 
 	this.widthProperty.addListener((_, _, newValue) => {
 		canvas.width = newValue.doubleValue() - playerPanel1.getWidth - playerPanel2.getWidth
-		canvas.drawMap(gameState.map)
-		canvas.drawUnits(gameState.units.toList, gameState.map)
-		canvas.drawSelection(gameState.map, new Hex(0, 0))
+		drawGame()
 	})
 
 	this.heightProperty.addListener((_, _, newValue) => {
 		canvas.height = newValue.doubleValue()
-		canvas.drawMap(gameState.map)
-		canvas.drawUnits(gameState.units.toList, gameState.map)
-		canvas.drawSelection(gameState.map, new Hex(0, 0))
+		drawGame()
 	})
 
 	this.setOnKeyPressed(ke => {
-		val char = ke.getCode
-		char match {
+		val keyCode = ke.getCode
+		keyCode match {
 			// Player 0
-			case KeyCode.Up.delegate => println(char.getName)
-			case KeyCode.Left.delegate => println(char.getName)
-			case KeyCode.Down.delegate => println(char.getName)
-			case KeyCode.Right.delegate => println(char.getName)
+			case KeyCode.Up.delegate => selectionY = selectionY - 1
+			case KeyCode.Left.delegate => selectionX = selectionX - 1
+			case KeyCode.Down.delegate => selectionY = selectionY + 1
+			case KeyCode.Right.delegate => selectionX = selectionX + 1
 			// Player 1
-			case KeyCode.W.delegate => println(char.getName)
-			case KeyCode.A.delegate => println(char.getName)
-			case KeyCode.S.delegate => println(char.getName)
-			case KeyCode.D.delegate => println(char.getName)
-			case _ => println(char.getName)
+			case KeyCode.W.delegate => selectionY = selectionY - 1
+			case KeyCode.A.delegate => selectionX = selectionX - 1
+			case KeyCode.S.delegate => selectionY = selectionY + 1
+			case KeyCode.D.delegate => selectionX = selectionX + 1
+			case _ => println(keyCode.getName)
 		}
 	})
 
