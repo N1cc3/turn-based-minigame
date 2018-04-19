f85f69f9cddd9e4a963b8d62361dba426cbc049a
Niclas Lindgren <niclaslindgren91@hotmail.com>
2018-04-19 22:13:49 +0300

    PlayerPanel UnitInfo now has nice hp bar.

--- src/main/scala/ui/ingame/UnitInfo.scala
+++ src/main/scala/ui/ingame/UnitInfo.scala
@@ -1,58 +1,68 @@
 package ui.ingame
 
 import game.data.Unit
-import scalafx.scene.control.Label
-import scalafx.scene.layout.{HBox, VBox}
+import scalafx.scene.control.{Label, ProgressBar}
+import scalafx.scene.layout.{StackPane, VBox}
 
 class UnitInfo extends VBox {
 
-	private val unitNameLabel = new Label()
-	unitNameLabel.styleClass += "UnitInfo-unitNameLabel"
-	this.children.add(unitNameLabel)
+	private val nameLabel = new Label()
+	nameLabel.styleClass += "UnitInfo-nameLabel"
+	this.children.add(nameLabel)
 
-	private val unitHpBox = new HBox()
-	unitHpBox.styleClass += "UnitInfo-unitHpBox"
-	this.children.add(unitHpBox)
+	private val hpBox = new StackPane()
+	hpBox.styleClass += "UnitInfo-hpBox"
+	this.children.add(hpBox)
 
-	private val unitHpLabel = new Label("12 / 15")
-	unitHpLabel.styleClass += "UnitInfo-unitHpLabel"
-	this.children.add(unitHpLabel)
+	private val hpBar = new ProgressBar()
+	hpBar.styleClass += "UnitInfo-hpBar"
+	hpBox.children.add(hpBar)
 
-	private val unitStatsBox = new VBox()
-	unitStatsBox.styleClass += "UnitInfo-unitStatsBox"
-	this.children.add(unitStatsBox)
+	private val hpLabel = new Label()
+	hpLabel.styleClass += "UnitInfo-hpLabel"
+	hpBox.children.add(hpLabel)
+
+	private val statsBox = new VBox()
+	statsBox.styleClass += "UnitInfo-statsBox"
+	this.children.add(statsBox)
 
 	private val attackRow = new StatRow("Attack")
-	unitStatsBox.children.add(attackRow)
+	statsBox.children.add(attackRow)
 
 	private val defenceRow = new StatRow("Defence")
-	unitStatsBox.children.add(defenceRow)
+	statsBox.children.add(defenceRow)
 
 	private val armorRow = new StatRow("Armor")
-	unitStatsBox.children.add(armorRow)
+	statsBox.children.add(armorRow)
 
 	private val rangeRow = new StatRow("Range")
-	unitStatsBox.children.add(rangeRow)
+	statsBox.children.add(rangeRow)
 
 	private val speedRow = new StatRow("Speed")
-	unitStatsBox.children.add(speedRow)
+	statsBox.children.add(speedRow)
+
+	clear()
 
 	def show(unit: Unit) {
-		unitNameLabel.text = unit.unitType.name
+		nameLabel.text = unit.unitType.name
 		attackRow.show(unit.unitType.attack)
 		defenceRow.show(unit.unitType.defence)
 		armorRow.show(unit.unitType.armor)
 		rangeRow.show(unit.unitType.range)
 		speedRow.show(unit.unitType.speed)
+		hpBar.setProgress(unit.hp / unit.unitType.hp)
+		hpLabel.text = unit.hp + " / " + unit.unitType.hp
 	}
 
 	def clear() {
-		unitNameLabel.text = ""
+		nameLabel.text = ""
 		attackRow.show(0)
 		defenceRow.show(0)
 		armorRow.show(0)
 		rangeRow.show(0)
 		speedRow.show(0)
+		hpBar.setProgress(0)
+		hpLabel.text = ""
 	}
 
 }
